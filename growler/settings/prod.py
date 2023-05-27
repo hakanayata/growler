47d24429ab628b9651c097b5649ac7e07a0e55cd
Hakan <s.hakanayata@gmail.com>
2023-05-27 13:58:20 +0200

    db setting update

--- growler/settings/prod.py
+++ growler/settings/prod.py
@@ -1,13 +1,25 @@
 from .base import *
+import MySQLdb
+
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("SQL_DATABASE"),
-        "USER": config("SQL_USER"),
-        "PASSWORD": config("SQL_PASSWORD"),
-        "HOST": config("SQL_HOST"),
-        "PORT": config("SQL_PORT"),
-        "ATOMIC_REQUESTS": True,
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config("SQL_DBNAME"),
+        'USER': config("SQL_USERNAME"),
+        'PASSWORD': config("SQL_PASSWORD"),
+        'HOST': config("SQL_HOST"),
     }
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": config("SQL_DATABASE"),
+#         "USER": config("SQL_USER"),
+#         "PASSWORD": config("SQL_PASSWORD"),
+#         "HOST": config("SQL_HOST"),
+#         "PORT": config("SQL_PORT"),
+#     }
+# }

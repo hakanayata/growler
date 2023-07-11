5608fd8fa159ae69ff6fd4100328b5645ebc7154
Hakan <s.hakanayata@gmail.com>
2023-07-11 11:13:08 +0200

    settings bug fix

--- growler/settings/base.py
+++ growler/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
